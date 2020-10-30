073fd76839f98f87a3a22258db814270546ffaa2
vdeb <vdebebe924@gmail.com>
2020-10-29 20:32:21 -0400

    create unittest for package, change package to work with ut

--- lambdata_vdeb/exoop_mod.py
+++ lambdata_vdeb/exoop_mod.py
@@ -1,7 +1,7 @@
 """Lambdata - a collection of Data Science helper functions
 with a class added"""
 
-#accessing libraries through pipenv
+
 import pandas as pd
 import numpy as np
 
@@ -12,17 +12,18 @@ class DfEda:
     
     def nv_search(self):
         """Cleans a DF"""
-        return (('?:',self.df.isin(['?']).sum()),
-            ('none:',self.df.isin(['none']).sum()),
-            ('null:',self.df.isin(['null']).sum()),
-            ('nan:',self.df.isin(['nan']).sum()))
+        return {'?': self.df.isin(['?']).sum().sum(),
+            'none': self.df.isin(['none']).sum().sum(),
+            'null': self.df.isin(['null']).sum().sum(),
+            'nan': self.df.isin(['nan']).sum().sum()}
 
     def tvt_split(self):
-        """Random Train, Val, Test on a DF"""
+        """Random 80/20/20 Train, Val, Test splits on a DataFrame.
+        Also returns the shapes of each split"""
         tvt = train, val, test = np.split(self.df.sample(frac=1), [int(.6*len(self.df)), int(.8*len(self.df))])
-        return (('Training Set:',train.shape),
-            ('Validation Set:',val.shape),
-            ('Test Set',test.shape))
+        return (('Training Set:', train.shape), 
+        ('Validation Set:', val.shape), 
+        ('Test Set:', test.shape))
 
 census = pd.read_csv("https://raw.githubusercontent.com/VPDeb/DS-Unit-2-Applied-Modeling/master/Build%20Week%20Project/census.csv")
 
--- lambdata_vdeb/test_ldvdeb.py
+++ lambdata_vdeb/test_ldvdeb.py
@@ -2,23 +2,42 @@
 import unittest
 from exoop_mod import DfEda
 import pandas as pd
+import numpy as np
+import random
 
 
 
 
 class CleanerTest(unittest.TestCase):
-    """Testing the Cleaner function to sum up various kinds of NaNs in DF"""
+    """Testing the Cleaner function to sum up various kinds of NaNs in DF and make 
+    train val test splits"""
     def setUp(self):
+        """Create three random dataframes to use in test"""
         self.df1= DfEda(pd.DataFrame({'a':[1, 'none', 3,'nan', 5, 2],
                                       'b':[4, 6, 8, '?', 'null', 9]}))
                               
         self.df2= DfEda(pd.DataFrame({'c':[6, 7, 'null', 9, 10, '?', 12, 5, 3, 2, 5],
                                    'd':[3, 5, '?', 2, 'none', '?', 6, 8, 4, 0, 'nan']}))
+        self.df3= DfEda(pd.DataFrame(np.random.randint(1, 101, size=(100, 2)), columns= list('ef')))
+
 
     def test_nanvalues(self):
-        self.assertIn(['?'].sum(), self.df1)
+        """Testing the nv_search function that will return which forms of 
+        nan values are in your dataframe"""
+        nan_dict = DfEda.nv_search(self.df1)
+        result = {'?': 1, 'none': 1, 'null': 1, 'nan': 1}
+        self.assertEqual(nan_dict, result)
 
-#self.df.isin(['?']).sum()
+        nan_dict2 = DfEda.nv_search(self.df2)
+        result = {'?': 3, 'none': 1, 'null': 1, 'nan': 1}
+        self.assertEqual(nan_dict2, result)
+
+    def test_tvtsplit(self):
+        """Testing to ensure the dataframe is randomly split into 
+        an 80/20/20 split for the train/val/test sets"""
+        split_df = DfEda.tvt_split(self.df3)
+        tvtt = (('Training Set:', (60, 2)), ('Validation Set:', (20, 2)), ('Test Set:', (20,2)))
+        self.assertEqual(split_df, tvtt)
 
 
 
